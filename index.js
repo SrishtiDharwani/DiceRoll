11a65eb3e168fa307ef7a4e35416cdd934e0f3e4
SrishtiDharwani <85086155+SrishtiDharwani@users.noreply.github.com>
2021-07-03 06:02:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+var randomNumber = Math.floor(Math.random() * 6) + 1;   //1-6
+var randomDiceImage = "images/dice" + randomNumber + ".png";      //images/dice1.png-images/dice6.png
+document.querySelectorAll("img")[0].setAttribute("src", randomDiceImage);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;       //1-6
+var randomDiceImage2 = "images/dice" + randomNumber2 + ".png";       //images/dice1.png-images/dice6.png
+document.querySelectorAll("img")[1].setAttribute("src", randomDiceImage2);
+
+if (randomNumber>randomNumber2)
+{
+  document.querySelector("h1").innerHTML="🚩 Player 1 Wins!";
+  document.querySelector("h3").innerHTML="Refresh Me!";
+}
+else if(randomNumber2>randomNumber)
+{
+  document.querySelector("h1").innerHTML="Player 2 Wins! 🚩";
+  document.querySelector("h3").innerHTML="Refresh Me!";
+}
+else
+{
+  document.querySelector("h1").innerHTML="Draw!";
+  document.querySelector("h3").innerHTML="Refresh Me!";
+}
